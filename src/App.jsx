eaa5c86d999288bdbe72419a1584916c0e4d2f5f
NasirMarwat <nasirkhani030859@gmail.com>
2024-03-15 16:10:12 +0500

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const UserName = "NasirMarwat";
+  return (
+    <>
+      <a href='https://wa.link/0keeio' target='_blank'>
+        Click to Connect with Muhammad Anwar WhatsApp
+      </a>
+      <h1>{UserName}</h1>
+    </>
+  );
+}
+export default App;
